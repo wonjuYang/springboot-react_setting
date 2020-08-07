dcc40308fd33c424b7daa455d4b7c84feb8f5411
triplej <his1monologue@gmail.com>
2020-08-07 18:16:31 +0900

    springboot+react setting1

---
+++ src/main/java/com/work/spbreact/SpbreactApplication.java
@@ -0,0 +1,13 @@
+package com.work.spbreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/work/spbreact/SpbreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.spbreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
